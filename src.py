5841a69dd4c97a21812d097c5b3fc8f9301dada4
Luis Aseguinolaza <luisasg_2001@hotmail.com>
2023-01-23 20:53:36 -0300

    grafico

--- src.py
+++ src.py
@@ -1,16 +1,26 @@
 import csv
 from coinmetrics.api_client import CoinMetricsClient
 import pandas
+import matplotlib.pyplot as plt
 
+def graph(data):
+    precio = []
+    for row in data:
+        precio = precio + [ row[1] ]
+    plt.plot( precio )
+    plt.ylabel('some numbers')
+    plt.show()
 
 def imprimir_csv():
-    
-    with open('Base_de_datos.csv', newline='') as f:
+    datos = []
+    with open('Base_de_datos.csv', newline='', encoding = "utf-16") as f:
         reader = csv.reader(f, delimiter='	', quotechar='|')
         i = 0
         for row in reader:
-            print("Numero de linea: ", i, row[0], row[1], row[2])
+            #print("Numero de linea: ", i, row[0], row[1], row[2])
+            datos = datos + [( row[0] , float(row[1]), float(row[2]) )]
             i += 1
+        graph(datos)
 
 def imprimir_api():
     client = CoinMetricsClient()
@@ -33,5 +43,5 @@ def imprimir_api():
     print(client)
     print(metrics)
 
-#imprimir_csv()
+imprimir_csv()
 imprimir_api()
