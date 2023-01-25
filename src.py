53b262aef8f74ae81b04cdfe06e031fae35b2259
Valentino <valentinobenedetich@gmail.com>
2023-01-24 21:25:19 -0300

    sfd

--- src.py
+++ src.py
@@ -3,6 +3,7 @@ from coinmetrics.api_client import CoinMetricsClient
 import pandas
 import matplotlib.pyplot as plt
 import math
+from datetime import date, timedelta
 
 def graph(data,headers): # data: es un dataframe de pandas. Lista con todos los "column headers" a graficar. Siempre se plotea en función del tiempo.
 
@@ -12,7 +13,7 @@ def graph(data,headers): # data: es un dataframe de pandas. Lista con todos los 
     for header in headers:
         print("hacemos el plot con el headder " ,header)
         plt.plot(xaxis, data[header] ,color, label = header)
-        color = color+1
+        color = color + 1
     
     #print(len(data3), " AAAAA ", len(data2))
 
@@ -43,26 +44,30 @@ def import_data_coinmetrics(asset_par, metrics_par):
     metrics = client.get_asset_metrics(assets= asset_par, 
                                         metrics = metrics_par,
                                         start_time = '2010-07-18',
-                                        #end_time = '2022-01-10',
+                                        end_time = date.today()- timedelta(1),
                                         #frequency = '1d'
                                         )
 
     metrics_P = pandas.DataFrame(metrics)
 
-    
-
-
-    print(metrics_P)
 
     def round_float(n):
         return round(float(n),2)
 
+
+    #correción de los datos:
+
     for el in metrics_par: #hace que todas las métricas importadas sean números en vez de texto, y los redondea a 2 decimales
         metrics_P[el] = list(map(round_float, metrics_P[el]))
 
-    print(metrics_P)
+    metrics_P['time'] = pandas.to_datetime(metrics_P['time'])
 
-    graph(metrics_P, ['PriceUSD', 'CapRealUSD'] ) #
+
+    return metrics_P
+
+def indicador_1(datos):
+
+    return datos
 
     #realized_price = []
     #i = 0
@@ -70,17 +75,18 @@ def import_data_coinmetrics(asset_par, metrics_par):
     #    realized_price = realized_price + [i]
     #    i += 1
   
-
     #metrics_2 = metrics_P.assign(PriceReal=realized_price)
-
-    #print(metrics_P)
-
 
     #for el in metrics_P['PriceReal']:
     #    print(el)
 
     #graph(metrics_P['PriceUSD'])
 
-#imprimir_csv()
 
-import_data_coinmetrics('btc', ['PriceUSD','CapRealUSD','SplyCur'])
+datos = import_data_coinmetrics('btc', ['PriceUSD','CapRealUSD','SplyCur'])
+
+datos_nuevo = indicador_1(datos)
+
+print(datos_nuevo)
+
+graph(datos, ['PriceUSD', 'CapRealUSD'] ) #
